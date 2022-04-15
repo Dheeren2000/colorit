8fcd7e062a63e22c74415b5ddd03a0e1f4bc6608
navneetdavang <navneetdavang@gmail.com>
2022-04-15 16:35:08 +0530

    changed file name

--- connectionmanager.py
+++ connectionmanager.py
@@ -14,6 +14,7 @@ class ConnectionManager:
     
     def getConnection(self):
         
+        # creating the connection object
         self.conn =  psycopg2.connect(host=self.DB_HOST,
                                     port=self.DB_PORT, 
                                     dbname=self.DB_NAME, 
